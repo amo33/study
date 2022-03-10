eca22a710a8fae2eaca16a438dea4368d0bce650
amo33 <andylee70044@gmail.com>
2022-03-10 09:02:04 +0900

    changes to be updated

--- react_django/Register_controller/frontend/src/components/example.js
+++ react_django/Register_controller/frontend/src/components/example.js
@@ -48,6 +48,5 @@ export default class InputSample extends Component {
         </Router>
         </div>
         );
-    }
+  }
 }
-
---
+++ react_django/Register_controller/frontend/src/components/register.js
@@ -0,0 +1,27 @@
+import React, { Component} from "react";
+import {render} from "react-dom";
+import {useState} from "react";
+function register(){
+
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const onReset = () => {
+        setText('');
+    };
+
+    return (
+        <div>
+        <input onChange={onChange} value={text}  />
+        <button onClick={onReset}>초기화</button>
+        <div>
+            <b>값: {text}</b>
+        </div>
+        </div>
+    );
+}
+
+export default register;
